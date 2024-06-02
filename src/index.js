77f7e886a3b6dae170e1a035376ff4f8e111e0ae
Arohan Ajit <arohanajit232@gmail.com>
2024-06-02 00:42:45 -0400

    fixed route

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/portfolio-sde" element={<App />} />
       </Routes>
     </Router>
   </React.StrictMode>
